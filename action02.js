7bdedbe7d153e36a54083e5c7723eaecb20afd5b
jay <devjay0530@gmail.com>
2021-07-27 15:30:48 +0900

    Update action02.js

--- action02.js
+++ action02.js
@@ -10,8 +10,8 @@ const octokit = new Octokit({
 
 
 octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-    owner: 'EntTangunGHEC',
-    repo: 'KakaoSeminar',
+    owner: 'lavinoys',
+    repo: 'actionsTest',
     issue_number: core.getInput('issue_number'),
     labels: [
       'bug'
